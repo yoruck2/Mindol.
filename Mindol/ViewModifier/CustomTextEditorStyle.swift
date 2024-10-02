5a1275b9fc9ad7038aaa1d1c3eabdfd019e10217
seunghyun Lee <yoruck2@gmail.com>
2024-10-03 00:58:21 +0900

    ✨ CustomTextEditorStyle 추가

--- Mindol/ViewModifier/CustomTextEditorStyle.swift
+++ Mindol/ViewModifier/CustomTextEditorStyle.swift
@@ -7,39 +7,41 @@
 
 import SwiftUI
 
+
+// MARK: 내용길어지면 늘어나는 TextEditor
+// TODO: 글자수 제한
 struct CustomTextEditorStyle: ViewModifier {
-    
     let placeholder: String
     @Binding var text: String
     
     func body(content: Content) -> some View {
-            content
-                .padding(15)
-                .background(alignment: .topLeading) {
-                    if text.isEmpty {
-                        Text(placeholder)
-                            .lineSpacing(10)
-                            .padding(20)
-                            .padding(.top, 2)
-                            .font(.system(size: 14))
-                            .foregroundColor(Color(UIColor.systemGray2))
-                    }
-                }
-                .autocorrectionDisabled()
-                .background(Color(UIColor.systemGray6))
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .scrollContentBackground(.hidden)
-                .font(.system(size: 14))
-                .overlay(alignment: .bottomTrailing) {
-                    Text("\(text.count)")
-                        .font(.system(size: 12))
+        content
+            .padding(15)
+            .background(alignment: .topLeading) {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .lineSpacing(10)
+                        .padding(20)
+                        .padding(.top, 2)
+                        .font(.system(size: 14))
                         .foregroundColor(Color(UIColor.systemGray2))
-                        .padding(.trailing, 15)
-                        .padding(.bottom, 15)
-                        .onChange(of: text) { newValue in
-                            text = String(newValue)
-                        }
                 }
+            }
+            .autocorrectionDisabled()
+            .background(Color(UIColor.systemGray6))
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .scrollContentBackground(.hidden)
+            .font(.system(size: 14))
+            .overlay(alignment: .topLeading) {
+                Text("\(text.count)")
+                    .font(.system(size: 12))
+                    .foregroundColor(Color(UIColor.systemGray2))
+                    .padding(.trailing, 15)
+                    .padding(.bottom, 15)
+                    .onChange(of: text) { newValue in
+                        text = String(newValue)
+                    }
+            }
     }
 }
 
