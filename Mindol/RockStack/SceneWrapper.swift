2bae1b1a39d9c8b46720001a24f8cbf00a5aa92d
seunghyun Lee <yoruck2@gmail.com>
2024-10-07 06:18:22 +0900

    🩹 현재 선택한 달이 바뀔때 scene을 업데이트 하도록 더 직관적인 수정

--- Mindol/RockStack/SceneWrapper.swift
+++ Mindol/RockStack/SceneWrapper.swift
@@ -10,60 +10,51 @@ import RealmSwift
 import Combine
 import FSCalendar
 class SceneWrapper: ObservableObject {
-    @Published var currentMonth: Date
+    @Published var currentMonth: Date {
+            didSet {
+                updateSceneForCurrentMonth()
+            }
+        }
        @Published var selectedDiaryId: ObjectId?
        private var scene: RockStackScene?
        let diaryRepository: DiaryRepository
     var calendarReference: FSCalendar?
-       init(diaryRepository: DiaryRepository) {
-           self.diaryRepository = diaryRepository
-           self.currentMonth = Date()
-           updateScene(for: currentMonth)
-       }
-    func refreshCalendar() {
-            calendarReference?.reloadData()
-            objectWillChange.send()
-        }
-    func refreshViews() {
-            updateScene(for: currentMonth)
-            calendarReference?.reloadData()
-            objectWillChange.send()
-        }
-        
-        func setCalendarReference(_ calendar: FSCalendar?) {
-            self.calendarReference = calendar
-        }
-       func updateScene(for date: Date) {
-           currentMonth = date
-           let diaries = diaryRepository.getDiariesForCurrentMonth(date: date)
-           scene?.setupRocksFromDiaries(diaries, for: date)
-           objectWillChange.send()
-       }
-       
-       func getScene() -> RockStackScene {
-           if let scene = scene {
-               return scene
-           }
-           let newScene = RockStackScene()
-           newScene.size = CGSize(width: 350, height: 450)
-           newScene.scaleMode = .aspectFill
-           newScene.onRockTapped = { [weak self] diaryId in
-               self?.selectedDiaryId = diaryId
-           }
-           self.scene = newScene
-           updateScene(for: currentMonth)
-           return newScene
-       }
     
-    func addSingleRock(_ diary: DiaryTable) {
+    init(diaryRepository: DiaryRepository) {
+            self.diaryRepository = diaryRepository
+            self.currentMonth = Date()
+        }
+    @Published var calendarNeedsUpdate: Bool = false
+
+        func updateCalendar() {
+            calendarNeedsUpdate = true
+            objectWillChange.send()
+        }
+        func updateSceneForCurrentMonth() {
+            let diaries = diaryRepository.getDiariesForCurrentMonth(date: currentMonth)
+            scene?.setupRocksFromDiaries(diaries, for: currentMonth)
+//            objectWillChange.send()
+        }
+
+        func getScene() -> RockStackScene {
+            if let scene = scene {
+                return scene
+            }
+            let newScene = RockStackScene()
+            newScene.size = CGSize(width: 350, height: 450)
+            newScene.scaleMode = .aspectFill
+            newScene.onRockTapped = { [weak self] diaryId in
+                self?.selectedDiaryId = diaryId
+            }
+            self.scene = newScene
+            updateSceneForCurrentMonth()
+            return newScene
+        }
+
+        func addSingleRock(_ diary: DiaryTable) {
             if Calendar.current.isDate(diary.date, equalTo: currentMonth, toGranularity: .month) {
                 scene?.addNewRock(diary)
                 objectWillChange.send()
             }
         }
-    
-    func updateScene() {
-        let diaries = diaryRepository.getDiariesForCurrentMonth(date: currentMonth)
-        scene?.setupRocksFromDiaries(diaries, for: currentMonth)
     }
-}
